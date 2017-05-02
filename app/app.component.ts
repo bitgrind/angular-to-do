98ee4307b331c542e593b3ae69a2741abd053be3
Keith Stedman <keithstedman@gmail.com>
2017-05-02 10:16:27 -0700

    added new functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,14 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="container">
-     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-     <h3>{{currentFocus}}</h3>
-     <ul>
-       <li>{{firstTask.description}}</li>
-     </ul>
-   </div>
-  `
+  <div class="container">
+    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
+     <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
+   </ul>
+   <hr>
+     <div>
+      <div *ngIf="selectedTask">
+        <h3>{{selectedTask.description}}</h3>
+        <p>Task Complete? {{selectedTask.done}}</p>
+        <h3>Edit Task</h3>
+        <label>Enter Task Description:</label>
+        <input [(ngModel)]="selectedTask.description">
+        <label>Enter Task Priority (1-3):</label>
+        <br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
+        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+        <button (click)="finishedEditing()">Done</button>
+      </div>
+    </div>
+  </div>
+    `
 })
 
 export class AppComponent {
@@ -19,10 +35,40 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-  firstTask: Task = new Task("Finish weekend Angular homework for Epicodus course");
+  tasks: Task[] = [
+      new Task('Finish weekend Angular homework for Epicodus course', 3),
+      new Task('Begin brainstorming possible JavaScript group projects', 2),
+      new Task('Add README file to last few Angular repos on GitHub', 2)  ];
+  selectedTask = null;
+
+  editTask(clickedTask) {
+    this.selectedTask = clickedTask;
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
+  }
+
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      console.log("This task is done!");
+    } else {
+      console.log("This task is not done. Better get to work!");
+    }
+  }
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3){
+     return "bg-danger";
+    } else if (currentTask.priority === 2) {
+     return  "bg-warning";
+    } else {
+     return "bg-info";
+    }
+  }
 }
 
 export class Task {
   public done: boolean = false;
-  constructor(public description: string){ }
+  constructor(public description: string, public priority: number) {   }
 }
